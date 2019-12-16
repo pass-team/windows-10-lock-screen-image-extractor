37e06e24e58b5c715fd3da8dc7f917c2277a79d1
duyphaphach <duyphamle97@gmail.com>
2019-12-16 11:09:01 +0700

    U1-8 Change file name of get-screen

--- source/index.js
+++ source/index.js
@@ -5,7 +5,7 @@ const {
   ORIENTATION_PORTRAIT,
   ORIENTATION_ALL,
 } = require('./constants');
-const getScreenAction = require('./actions/get-screen');
+const getLockScreenImageAction = require('./actions/get-lock-screen-image');
 
 // App construction
 app
@@ -13,6 +13,6 @@ app
   .description('Extract the mysterious Windows 10 lock screens and save to the folder of your choice')
   .argument('[orientation]', 'Choose image orientation', [ORIENTATION_LANDSCAPE, ORIENTATION_PORTRAIT, ORIENTATION_ALL], ORIENTATION_ALL)
   .option('-p, --path', '\tPath to saving folder\t', /[A-Z]:.+|false/, DEFAULT_SAVE_PATH, false)
-  .action(getScreenAction);
+  .action(getLockScreenImageAction);
 
 app.parse(process.argv);
