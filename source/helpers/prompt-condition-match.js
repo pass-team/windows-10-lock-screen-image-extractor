5a607979918af3f99d6bcd294b17e9f6cab9fd3c
alexander-pham <duy@passcareer.io>
2020-01-15 14:55:57 +0700

    U1-24 Add comments for helper: prompt-condition-match

--- source/helpers/prompt-condition-match.js
+++ source/helpers/prompt-condition-match.js
@@ -1,3 +1,10 @@
+/**
+ *  @Helper
+ *  @Input:
+ *    - process: the current process run by node
+ *  @Output:
+ *    - return: an array of raw inputs from user
+ */
 module.exports = function (process) {
   return process.argv.splice(2).length <= 0;
 };
