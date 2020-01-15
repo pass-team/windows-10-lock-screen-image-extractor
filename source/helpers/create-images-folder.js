370ad27b0c9a9cbd5b958f34d5e1070f7c43c5c1
alexander-pham <duy@passcareer.io>
2020-01-15 12:39:19 +0700

    U1-24 Add comments for helper: create-images-folder

--- source/helpers/create-images-folder.js
+++ source/helpers/create-images-folder.js
@@ -1,5 +1,13 @@
 const fs = require('fs');
 
+/**
+ *  @Helper
+ *  @Input:
+ *    - path: path to image folder
+ *  @Output:
+ *    - true if create successfully or EEXIST exception occurs: means the folder already existed
+ *    - false otherwise
+ */
 module.exports = function (path) {
   try {
     fs.mkdirSync(path);
