4f386f066f8830052ba397534bf5e896fa704c05
alexander-pham <duy@passcareer.io>
2020-01-15 12:01:41 +0700

    U1-24 Add comments for action: show-settings

--- source/actions/show-settings.js
+++ source/actions/show-settings.js
@@ -3,6 +3,7 @@ const {
   getSavePath,
 } = require('../helpers');
 
+/* Action that retrieve user's configurations and display */
 module.exports = function (args, options, logger) {
   const currentSavePath = getSavePath();
   logger.info('\nImage save folder (Ctrl + click to open):');
