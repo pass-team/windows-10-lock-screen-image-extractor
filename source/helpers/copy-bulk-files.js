0d78a7c9951febe3d5159a518609fe71df66ab01
alexander-pham <duy@passcareer.io>
2020-01-15 12:21:49 +0700

    U1-24 Add comments for helper: copy-bulk-files

--- source/helpers/copy-bulk-files.js
+++ source/helpers/copy-bulk-files.js
@@ -1,5 +1,19 @@
 const copyFile = require('./copy-file');
 
+/**
+ *  @Helper
+ *  @Input:
+ *    - files: An array of file meta objects
+ *    - src: path to source folder
+ *    - dest: path to destination folder
+ *    - pattern: file name format to be save as, presently support
+ *      - hash: content hash
+ *      - date: current date
+ *      - origin: the original file name
+ *  @Output:
+ *    - files are copy to dest folder
+ *    - return the number of files copied
+ */
 module.exports = function (files, src, dest, pattern) {
   return files.reduce((count, file, index) => {
     try {
