090b294bed8a160700d26fef11c3498b686817de
Ho Quang Huy <huyat02@gmail.com>
2020-01-31 17:22:52 +0700

    add package nixt and config jest

---
+++ config/jest/integration/jest.config.js
@@ -0,0 +1,6 @@
+/* eslint quotes: ["error", "single"] */
+module.exports = {
+  rootDir: './../../..',
+  cacheDirectory: '<rootDir>/.cache/jest',
+  testMatch: ['<rootDir>/tests/integration/**/*.test.js'],
+};
