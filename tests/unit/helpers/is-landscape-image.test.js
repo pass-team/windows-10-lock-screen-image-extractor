6ea9c6e5c45546ec210534f7143ca114d1051e3e
Ho Quang Huy <huyat02@gmail.com>
2020-02-04 15:23:04 +0700

    U1-46 Write unit test for function is-portrait-image

---
+++ tests/unit/helpers/is-landscape-image.test.js
@@ -0,0 +1,16 @@
+const isPortraitImage = require('../../../source/helpers/is-portrait-image');
+
+describe('Helper - Function is-portrait-image', () => {
+  it('Should return true if an image is portrait', () => {
+    expect(isPortraitImage({
+      height: 1366,
+      width: 768
+    })).toEqual(true);
+  });
+  it('Should return false if an image is not portrait', () => {
+    expect(isPortraitImage({
+      height: 768,
+      width: 1366
+    })).toEqual(false);
+  });
+});
