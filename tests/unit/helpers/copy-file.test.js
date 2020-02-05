df054c2b6865b07f875d7a5f32347a954cf41861
Ho Quang Huy <huyat02@gmail.com>
2020-02-05 18:52:11 +0700

    U1-57 Update variables and add test cases to test function copy-file

--- tests/unit/helpers/copy-file.test.js
+++ tests/unit/helpers/copy-file.test.js
@@ -2,34 +2,50 @@ const fs = require('fs');
 const copyFile = require('../../../source/helpers/copy-file');
 const createImagesFolder = require('../../../source/helpers/create-images-folder');
 const normalizePath = require('../../../source/helpers/normalize-path');
-const mockPathToImages = `${process.cwd()}/tests/mock-assets/`;
 const deleteFolderRecursive = require('../../mock-data/delete-folder-recursive');
 
 describe('Helper - Function copy-file', () => {
-  const path = 'D://images';
+  const mockSource = `${process.cwd()}/tests/mock-assets/`;
+  const mockDestination = `${process.cwd()}/tests/mock-destination/`;
+  let mockNamePattern = 'origin';
+  const images = {
+    name: 'a',
+    path: mockDestination,
+    height: 1920,
+    width: 1080,
+    type: 'jpg',
+    origin: 'a',
+    hash: 'db7ee7b47dd509ac37bc930322e0f1b3',
+    date: 'February 5 2020'
+  }
 
   beforeEach(() => {
-    createImagesFolder(path);
+    createImagesFolder(mockDestination);
   })
 
   afterEach(() => {
-    deleteFolderRecursive(path);
+    deleteFolderRecursive(mockDestination);
   });
-  it('Should copy file with correct name pattern', () => {
-    const images = {
-      name: 'a',
-      path: mockPathToImages,
-      height: 1920,
-      width: 1080,
-      type: 'jpg',
-      origin: 'a',
-      hash: 'db7ee7b47dd509ac37bc930322e0f1b3',
-      date: 'February 5 2020'
-    };
-
-    copyFile(images, mockPathToImages, path, 'origin')
-    const copyImages = fs.readdirSync(normalizePath(path));
+  it('Should copy file with name pattern is origin', () => {
+    copyFile(images, mockSource, mockDestination, mockNamePattern);
+    const copyImages = fs.readdirSync(normalizePath(mockDestination));
     
-    expect(copyImages).toEqual(['a.jpg']);
+    expect(copyImages).toEqual([`${images.name}.${images.type}`]);
+  });
+
+  it('Should copy file with name pattern is date', () => {
+    mockNamePattern = 'date';
+    copyFile(images, mockSource, mockDestination, mockNamePattern, 1);
+    const copyImages = fs.readdirSync(normalizePath(mockDestination));
+    
+    expect(copyImages).toEqual([`${images.date}_1.${images.type}`]);
+  });
+
+  it('Should copy file with name pattern is hash', () => {
+    mockNamePattern = 'hash';
+    copyFile(images, mockSource, mockDestination, mockNamePattern, 1);
+    const copyImages = fs.readdirSync(normalizePath(mockDestination));
+    
+    expect(copyImages).toEqual([`${images.hash}.${images.type}`]);
   });
 });
