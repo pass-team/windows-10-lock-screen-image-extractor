417e6bec3066c3fd138ff345848d01014a383867
Ho Quang Huy <huyat02@gmail.com>
2020-02-05 11:04:30 +0700

    U1-49 Write unit test for function create-images-folder

---
+++ tests/unit/helpers/create-images-folder.test.js
@@ -0,0 +1,11 @@
+const createImagesFolder = require('../../../source/helpers/create-images-folder');
+
+describe('Helper - Function create-images-folder', () => {
+  it('Should return true if create successfully', () => {
+    expect(createImagesFolder('D://lock-screen-images')).toEqual(true);
+  });
+
+  it('Should return false if do not create successfully', () => {
+    expect(createImagesFolder('D:/')).toEqual(false);
+  });
+});
