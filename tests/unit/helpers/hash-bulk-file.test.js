f26291e57b75506d78417339cb75ba36017a707c
duyphaphach <duy@passcareer.io>
2020-02-05 13:22:04 +0700

    U1-54 Write units test for helper function hash-bulk-file

---
+++ tests/unit/helpers/hash-bulk-file.test.js
@@ -0,0 +1,22 @@
+const hashBulkFile = require('../../../source/helpers/hash-bulk-file');
+const getFiles = require('../../../source/helpers/get-files');
+
+describe('Helper - Function hash-file', () => {
+  it('Should return the file hash as string', () => {
+    // Get files from mock assets
+    const files = getFiles(`${process.cwd()}/tests/mock-assets`);
+    const expectedHashes =  [
+      'db7ee7b47dd509ac37bc930322e0f1b3',
+      'cd1e461dccf6b9f55a0de6b195662c0a',
+      'ce81b0a1dcd69805c69957c5d632a153',
+      'f5efb8f08a895f4f5d8a69cb0200f224',
+      'd62143732a7a3e0135dc8f6348cc5cbe',
+      '55706427975cf355a9e0071790c95f21'
+    ];
+
+    const hashes = hashBulkFile(files);
+    expect(hashes).toBeTruthy();
+    expect(Array.isArray(hashes)).toBe(true);
+    expect(hashes).toEqual(expectedHashes);
+  });
+});
