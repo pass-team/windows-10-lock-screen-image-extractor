da4b78123e09689575cb4640cd78bb2a0e940ff9
Ho Quang Huy <huyat02@gmail.com>
2020-02-06 15:47:15 +0700

    U1-59 Write unit test for helper function prompt-condition-match

---
+++ tests/unit/helpers/prompt-condition-match.test.js
@@ -0,0 +1,24 @@
+const promptConditionMatch = require('../../../source/helpers/prompt-condition-match');
+
+describe('Helper - Function prompt-condition-match', () => {
+  it('Should return true when the process has no argument', () => {
+    process = {
+      argv: [
+        'a',
+        'b'
+      ]      
+    }
+    expect(promptConditionMatch(process)).toBeTruthy();
+  });
+  it('Should return false when the process has more than one arguments', () => {
+    process = {
+      argv: [
+        'a',
+        'b',
+        'c'
+      ]      
+    }
+    expect(promptConditionMatch(process)).toBeFalsy();
+  });
+});
+
