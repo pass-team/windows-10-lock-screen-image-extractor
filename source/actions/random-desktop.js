4f76b2456548cd8291fd20a54b1585ceb4bfb95b
duyphaphach <duy@passcareer.io>
2020-02-07 16:33:34 +0700

    U1-69 Feature random desktop show wrong message on first run

--- source/actions/random-desktop.js
+++ source/actions/random-desktop.js
@@ -12,7 +12,10 @@ export default async function (args, options, logger) {
   logger.info(chalk.cyan('\nStart processing'));
   /* 1. Retrieve image saving path, stop if no save path found */
   const currentSavePath = await taskExecutor(getSavePath(), 'Checking saved images..', 400);
-  if (!currentSavePath) return;
+  if (!currentSavePath) {
+    logger.warn(chalk.yellow('\nNo existing images, try getting the images first, run "get-lock-screen -h" for usage'));
+    return;
+  }
   /* 2. Retrieve saved images */
   const savedImages = getFiles(currentSavePath);
 
