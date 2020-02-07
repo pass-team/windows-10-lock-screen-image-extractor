8de602b12374285842991d7575f56b721ec77410
Ho Quang Huy <huyat02@gmail.com>
2020-02-07 11:17:02 +0700

    U1-56 Add test case to increase code coverage

--- tests/unit/helpers/copy-bulk-files.test.js
+++ tests/unit/helpers/copy-bulk-files.test.js
@@ -1,4 +1,4 @@
-const hashFile = require('../../../source/helpers/hash-file');
+const { hashFile } = require('../../../source/helpers');
 const creatImagesFolder = require('../../../source/helpers/create-images-folder');
 const copyBulkFiles = require('../../../source/helpers/copy-bulk-files');
 const deleteFolderRecursive = require('./../../mock-data/delete-folder-recursive');
@@ -7,13 +7,12 @@ describe('Helper - Function copy-bulk-files', () => {
   const mockSource = `${process.cwd()}/tests/mock-assets/`
   const mockDestination = `${process.cwd()}/tests/mock-destination/`;
   const mockNamePatter = 'origin';
+  let files;
 
-  it('Should return the number of copied files', () => {
-    creatImagesFolder(mockDestination);
-
+  beforeEach(() => {
     /** Mock file meta objects represent mock assets folder
      * to avoid having to run other helpers */
-    const files = new Array(6).fill().map((e, index) => {
+    files = new Array(6).fill().map((e, index) => {
       const originalFileName = String.fromCharCode(97 + index);
       return {
         name: originalFileName,
@@ -26,7 +25,10 @@ describe('Helper - Function copy-bulk-files', () => {
         date: 'February 5 2020'
       }
     });
+    creatImagesFolder(mockDestination);
+  })
 
+  it('Should return the number of copied files', () => {
     // Copy first time
     const copyCount = copyBulkFiles(files, mockSource, mockDestination, mockNamePatter);
     expect(copyCount).toEqual(6);
@@ -34,6 +36,12 @@ describe('Helper - Function copy-bulk-files', () => {
     // Copy second time, duplicated files so return 0
     const copyCountSecond = copyBulkFiles(files, mockSource, mockDestination, mockNamePatter);
     expect(copyCountSecond).toEqual(0);
+  });
+
+  it('Should return false if fail to copy', () => {
+    jest.spyOn(console, 'log').mockReturnValue('');
+    const copyCount = copyBulkFiles(files, '', mockDestination, mockNamePatter);
+    expect(copyCount).toBeFalsy();
   });
 
   afterEach(() => {
