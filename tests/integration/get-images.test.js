da4218b57ec99e4271af02cba6f7d9cfaa8e1b0a
duyphaphach <duy@passcareer.io>
2020-02-09 18:02:26 +0700

    U1-69 Refractor integration tests to adapt new build path

--- tests/integration/get-images.test.js
+++ tests/integration/get-images.test.js
@@ -1,20 +1,27 @@
 import nixt from 'nixt/lib/nixt/runner';
+import path from 'path';
 
 describe('Feature get-images', () => {
-  it('Should get images with default settings', function(done) {
+  const cwd = path.join(process.cwd(), '/build');
+
+  it('Should get images with default settings', function (done) {
     nixt()
-    .stdin('\n')
-    .run('get-lock-screen get-images')
-    .stdout(/.*Save folder.*/)
-    .end(done);
+      .cwd(cwd)
+      .exec('cd build')
+      .stdin('\n')
+      .run('get-lock-screen get-images')
+      .stdout(/.*Save folder.*/)
+      .end(done);
   });
 
-  it('Should get images with additional arguments', function(done) {
+  it('Should get images with additional arguments', function (done) {
     const folderName = Math.floor(Math.random() * Math.floor(10000));
     nixt()
-    .stdin('\n')
-    .run(`get-lock-screen get-images -p=D:/${folderName} -o=landscape -n=hash`)
-    .stdout(/.*Save folder.*/)
-    .end(done);
+      .cwd(cwd)
+      .exec('cd build')
+      .stdin('\n')
+      .run(`get-lock-screen get-images -p=D:/${folderName} -o=landscape -n=hash`)
+      .stdout(/.*Save folder.*/)
+      .end(done);
   });
 });
--- tests/integration/show-settings.test.js
+++ tests/integration/show-settings.test.js
@@ -1,16 +1,23 @@
 import nixt from 'nixt/lib/nixt/runner';
+import path from "path";
 
 describe('Feature show-settings', () => {
+  const cwd = path.join(process.cwd(), '/build');
+
   it('Should display path to saving folder', (done) => {
     nixt()
-    .stdin('\n')
-    .run('get-lock-screen get-images')
-    .end(done);
+      .cwd(cwd)
+      .exec('cd build')
+      .stdin('\n')
+      .run('get-lock-screen get-images')
+      .end(done);
 
     nixt()
-    .stdin('\n')
-    .run('get-lock-screen show-settings')
-    .stdout(/.*Pictures\\W10_Spotlight\\.*/)
-    .end(done);
+      .cwd(cwd)
+      .exec('cd build')
+      .stdin('\n')
+      .run('get-lock-screen show-settings')
+      .stdout(/.*Pictures\\W10_Spotlight\\.*/)
+      .end(done);
   });
 });
