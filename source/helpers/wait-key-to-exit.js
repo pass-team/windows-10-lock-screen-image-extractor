1d8397651d7d89adeb4af53bd58b7a00123ca8f1
duyphaphach <duy@passcareer.io>
2020-02-11 14:47:31 +0700

    U1-75 Update code waitKeyToExit

--- source/helpers/wait-key-to-exit.js
+++ source/helpers/wait-key-to-exit.js
@@ -4,7 +4,9 @@ import chalk from 'chalk';
 
 module.exports = function () {
   console.log(chalk.cyan('\nPress any key to exit..'));
-  process.stdin.setRawMode(true);
+  if (process.stdin.isTTY) {
+    process.stdin.setRawMode(true);
+  }
   process.stdin.resume();
   process.stdin.on('data', process.exit.bind(process, 0));
 };
