3603bb17e4df956bc27fe904d561d5780f2b4d8d
Ho Quang Huy <huyat02@gmail.com>
2020-02-12 15:28:32 +0700

    U1-79 Write unit test for action show-menu

--- tests/unit/actions/show-menu.test.js
+++ tests/unit/actions/show-menu.test.js
@@ -6,11 +6,16 @@ import {
 import {
   showMenu,
 } from '../../../source/actions';
+import showSettings from '../../../source/actions/show-settings';
+import getImages from '../../../source/actions/get-images';
+import randomDesktop from '../../../source/actions/random-desktop';
 import deleteFolderRecursive from '../../mock-data/delete-folder-recursive';
 
 jest.mock('caporal/lib/logger');
 jest.mock('enquirer/lib/prompts/select');
 jest.mock('../../../source/actions/get-images');
+jest.mock('../../../source/actions/show-settings');
+jest.mock('../../../source/actions/random-desktop');
 
 let infoRecord = '';
 let warnRecord = '';
@@ -41,22 +46,54 @@ describe('Action show-menu', () => {
     infoRecord = '';
     warnRecord = '';
   });
-  it('Should call action get-images when choose "3. Show current user settings"', async () => {
-    const folder = 'D://screen-images';
+  it('Should call action get-images when choose "1. Get lock screen images"', async () => {
     const logger = mockLogger();
-
     Select.mockImplementation(() => {
       return {
-        run: jest.fn().mockResolvedValue(MENU_OPTIONS.CURRENT_SETTINGS),
+        run: jest.fn().mockResolvedValue(MENU_OPTIONS.GET_LOCK_SCREEN),
       }
-    })
+    });
+
+    getImages.mockImplementation(() => '');
     await showMenu(
       {},
       {},
       logger
     );
-    expect(infoRecord.includes('Image saved folder(Ctrl + click to open):')).toBeTruthy();
-    expect(infoRecord.includes(`file://${folder}`)).toBeTruthy();
-    deleteFolderRecursive(folder);
+    expect(getImages).toHaveBeenCalled();
+  });
+
+  it('Should call action random-desktop when choose "2. Randomize desktop background"', async () => {
+    const logger = mockLogger();
+    Select.mockImplementation(() => {
+      return {
+        run: jest.fn().mockResolvedValue(MENU_OPTIONS.RANDOM_DESKTOP),
+      }
+    });
+
+    randomDesktop.mockImplementation(() => '');
+    await showMenu(
+      {},
+      {},
+      logger
+    );
+    expect(randomDesktop).toHaveBeenCalled();
+  });
+
+  it('Should call action show-settings when choose "3. Show current user settings"', async () => {
+    const logger = mockLogger();
+    Select.mockImplementation(() => {
+      return {
+        run: jest.fn().mockResolvedValue(MENU_OPTIONS.CURRENT_SETTINGS),
+      }
+    });
+
+    showSettings.mockImplementation(() => '');
+    await showMenu(
+      {},
+      {},
+      logger
+    );
+    expect(showSettings).toHaveBeenCalled();
   });
 });
