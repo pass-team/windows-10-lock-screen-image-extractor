a3b15329334cd982ebba44fa689d486d5a80e040
duyphaphach <duy@passcareer.io>
2020-02-12 16:57:36 +0700

    U1-81 Update fix for test timeout

--- tests/unit/actions/random-desktop.test.js
+++ tests/unit/actions/random-desktop.test.js
@@ -1,35 +1,108 @@
+jest.mock('caporal/lib/logger');
 import fs from 'fs';
 import path from 'path';
 import wallpaper from 'wallpaper';
-import {IMAGE_NAME_FORMAT_HASH, ORIENTATION_LANDSCAPE} from '../../../source/constants';
+import chalk from 'chalk';
+import logger from 'caporal/lib/logger';
+import {
+  IMAGE_NAME_FORMAT_HASH,
+  ORIENTATION_ALL,
+  ORIENTATION_LANDSCAPE,
+  PATH_TO_CONFIG
+} from '../../../source/constants';
 import getImages from '../../../source/actions/get-images';
 import randomDesktop from '../../../source/actions/random-desktop';
 import deleteFolderRecursive from './../../mock-data/delete-folder-recursive';
 
-describe('Feature random-desktop', () => {
+let infoRecord = '';
+let warnRecord = '';
+let errorRecord = '';
+
+const mockLogger = logger.createLogger.mockImplementation(() => {
+  return {
+    info: jest.fn(
+      function (data) {
+        infoRecord += data;
+      }
+    ),
+    warn: jest.fn(
+      function (data) {
+        warnRecord += data;
+      }
+    ),
+    error: jest.fn(
+      function (data) {
+        errorRecord += data;
+      }
+    )
+  }
+});
+
+describe('Action - Function random-desktop', () => {
+  // Reset logging recorder
+  beforeEach(() => {
+    infoRecord = '';
+    warnRecord = '';
+  });
   // Run test inside build folder
   // Remove user settings to avoid side effect on other test cases
   afterEach(() => {
-    if(fs.existsSync(path.join(process.cwd(), '\\.userconfig'))) {
+    if (fs.existsSync(path.join(process.cwd(), '\\.userconfig'))) {
       fs.unlinkSync(path.join(process.cwd(), '\\.userconfig'));
     }
+    deleteFolderRecursive('D:/w10-startup-lock-screen-extractor/');
   });
 
-  it('Should set new desktop wallpaper', async () => {
-    const folder = `D:/w10-startup-lock-screen-extractor-random-desktop-folder/${Math.floor(Math.random() * Math.floor(10000))}/`;
-    const logger = {
-      info: jest.fn().mockReturnValue(''),
-      warn: jest.fn().mockReturnValue(''),
-      error: jest.fn().mockReturnValue(''),
-    };
-
-    await getImages({}, { path: folder, orientation: ORIENTATION_LANDSCAPE, namePattern: IMAGE_NAME_FORMAT_HASH }, logger);
-    await randomDesktop({}, { path: folder, orientation: ORIENTATION_LANDSCAPE, namePattern: IMAGE_NAME_FORMAT_HASH }, logger);
-
+  it('Should be able to set new desktop wallpaper after successfully get images ', async () => {
+    const folder = `D:/w10-startup-lock-screen-extractor/${Math.floor(Math.random() * Math.floor(10000))}/`;
+    const logger = mockLogger();
+    await getImages({}, {
+      path: folder,
+      orientation: ORIENTATION_LANDSCAPE,
+      namePattern: IMAGE_NAME_FORMAT_HASH
+    }, logger);
+    await randomDesktop({}, {}, logger);
     const images = fs.readdirSync(folder);
     const wallpaperName = path.basename(await wallpaper.get());
-    expect(images.includes(wallpaperName)).toBe(true);
-    // Clean up trash files created by test case
-    deleteFolderRecursive(folder);
+    // Expect images exists images folder
+    // And it display success message
+    expect(images.includes(wallpaperName)).toBeTruthy();
+    expect(infoRecord.includes('New desktop background has been set')).toBeTruthy();
+  });
+
+  it('Should display “No existing images…“ when image folder does not exist', async () => {
+    const logger = mockLogger();
+    // Mock an empty .userconfig file, which should be containing path to image folder
+    fs.writeFileSync(PATH_TO_CONFIG, '');
+    await randomDesktop({}, {}, logger);
+    expect(infoRecord).toEqual(chalk.cyan('\nStart processing'));
+    expect(warnRecord).toEqual(chalk.yellow('\nNo existing images, try getting the images first, run "get-lock-screen -h" for usage'));
+  });
+
+  it('Should display “No existing images…“ when found no image to use', async () => {
+    const folder = `D:/w10-startup-lock-screen-extractor/${Math.floor(Math.random() * Math.floor(10000))}/`;
+    const logger = mockLogger();
+    // Mock an empty .userconfig file, which should be containing path to image folder
+    fs.writeFileSync(PATH_TO_CONFIG, folder);
+    await randomDesktop({}, {}, logger);
+    expect(warnRecord).toEqual(chalk.yellow('\nNo existing images, try getting the images first, run "get-lock-screen -h" for usage'));
+  });
+
+  it('Should display “Unexpected errors…“ when fail to set wallpaper for some reasons', async () => {
+    // Mock process.argv to bypass prompt which leads to timeOut
+    const logger = mockLogger();
+    const oldProcessArgv = process.argv;
+    process.argv = [ ...oldProcessArgv, '', ''];
+    await getImages({}, { }, logger);
+    // Mock error while retrieving setWallpaper binary
+    const old = fs.writeFileSync;
+    fs.writeFileSync = jest.fn(function mock() {
+      throw 'Unexpected Error'
+    });
+    await randomDesktop({}, {}, logger);
+    // Mock an empty .userconfig file, which should be containing path to image folder
+    expect(warnRecord).toEqual(chalk.yellow('\nUnexpected errors!'));
+    fs.writeFileSync = old;
+    process.argv = oldProcessArgv;
   });
 });
