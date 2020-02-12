e37c29ccaa88754d78f334d6ca75701bfcadf2e9
Ho Quang Huy <huyat02@gmail.com>
2020-02-12 17:04:20 +0700

    U1-80 Update test case

--- tests/unit/actions/show-settings.test.js
+++ tests/unit/actions/show-settings.test.js
@@ -2,15 +2,44 @@ import fs from 'fs';
 import path from 'path';
 import {
   IMAGE_NAME_FORMAT_HASH,
-  ORIENTATION_LANDSCAPE,
-  PATH_TO_CONFIG
+  ORIENTATION_LANDSCAPE
 } from '../../../source/constants';
 import getImages from '../../../source/actions/get-images';
 import showSettings from '../../../source/actions/show-settings';
-import normalizePath from '../../../source/helpers/normalize-path';
+import logger from 'caporal/lib/logger';
 import deleteFolderRecursive from '../../mock-data/delete-folder-recursive';
 
+jest.mock('caporal/lib/logger');
+
+let infoRecord = '';
+let warnRecord = '';
+let errorRecord = '';
+
+const mockLogger = logger.createLogger.mockImplementation(() => {
+  return {
+    info: jest.fn(
+      function (data) {
+        infoRecord += data;
+      }
+    ),
+    warn: jest.fn(
+      function (data) {
+        warnRecord += data;
+      }
+    ),
+    error: jest.fn(
+      function (data) {
+        errorRecord += data;
+      }
+    )
+  }
+});
+
 describe('Feature show-settings', () => {
+  beforeEach(() => {
+    infoRecord = '';
+    warnRecord = '';
+  });
   // Run test inside build folder
   // Remove user settings to avoid side effect on other test cases
   afterEach(() => {
@@ -19,25 +48,25 @@ describe('Feature show-settings', () => {
     }
   });
 
-  it('Should return path to image saving folder', async () => {
+  it('Should display "No user settings has been recorded yet.." when .userconfig does not exist', async () => {
+    const logger = mockLogger();
+    await showSettings({}, {}, logger);
+
+    expect(warnRecord.includes('No user settings has been recorded yet')).toBeTruthy();
+  });
+
+  it('Should print out path to images', async () => {
+    const logger = mockLogger();
     const folder = 'D://screen-images';
     const answers = {
       path: folder,
       orientation: ORIENTATION_LANDSCAPE,
       namePattern: IMAGE_NAME_FORMAT_HASH
     };
-    const logger = {
-      info: jest.fn().mockReturnValue(''),
-      warn: jest.fn().mockReturnValue(''),
-      error: jest.fn().mockReturnValue(''),
-    };
-
     await getImages({}, answers, logger);
-    await showSettings({}, answers, logger);
+    await showSettings({}, {}, logger);
 
-    const currentSavePath = await fs.readFileSync(PATH_TO_CONFIG).toString();
-
-    expect(currentSavePath).toEqual(normalizePath(folder));
+    expect(infoRecord.includes(`file://${folder}`)).toBeTruthy();
     // Clean up trash files created by test case
     deleteFolderRecursive(folder);
   });
