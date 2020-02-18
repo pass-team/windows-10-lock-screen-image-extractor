7457ba0be33c1fdb679999cfc26f1f9da3da0dbd
huyhq <huyat02@gmail.com>
2020-02-18 18:24:50 +0700

    U1-92 Write unit test for helper function set

---
+++ tests/unit/helpers/set-wallpaper.test.js
@@ -0,0 +1,15 @@
+import wallpaper from 'wallpaper';
+import fs from 'fs';
+import path from 'path';
+import setWallpaper from '../../../source/helpers/set-wallpaper';
+
+describe('Helper - Function set-wallpaper', () => {
+  it('Should be able to set new desktop wallpaper after successfully set wallpaper', async () => {
+    const mockPath = `${process.cwd()}/tests/mock-assets/`;
+    const image = `${mockPath}/a`;
+    const images = fs.readdirSync(mockPath);
+    setWallpaper(image);
+    const wallpaperName = path.basename(await wallpaper.get());
+    expect(images.includes(wallpaperName)).toBeTruthy();
+  });
+});
