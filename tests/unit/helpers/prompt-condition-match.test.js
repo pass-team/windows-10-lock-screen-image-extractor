3b422533a97a7361a9e61f8a85e69cccaf99133f
duyphaphach <duy@passcareer.io>
2020-02-27 12:32:46 +0700

    U1-90 Update test for Helper - prompt-condition-match

--- tests/unit/helpers/prompt-condition-match.test.js
+++ tests/unit/helpers/prompt-condition-match.test.js
@@ -15,4 +15,33 @@ describe('Helper - Function prompt-condition-match', () => {
 
     expect(promptConditionMatch(process)).toBeFalsy();
   });
+
+  it('Should return true when the process has only one argument: verbose', () => {
+    process.argv = ['a', 'b', '-v'];
+    expect(promptConditionMatch(process)).toBeTruthy();
+
+    process.argv = ['a', 'b', '--verbose'];
+    expect(promptConditionMatch(process)).toBeTruthy();
+
+    process.argv = ['a', 'b', 'get-images', '--verbose'];
+    expect(promptConditionMatch(process)).toBeFalsy();
+
+    process.argv = ['a', 'b', 'get-images', '-p', 'D:/', '--verbose'];
+    expect(promptConditionMatch(process)).toBeFalsy();
+
+    process.argv = ['a', 'b', '--verbose', 'get-images', '-p', 'D:/'];
+    expect(promptConditionMatch(process)).toBeFalsy();
+
+    process.argv = ['a', 'b', '-v'];
+    expect(promptConditionMatch(process)).toBeTruthy();
+
+    process.argv = ['a', 'b', 'get-images', '-v'];
+    expect(promptConditionMatch(process)).toBeFalsy();
+
+    process.argv = ['a', 'b', 'get-images', '-p', 'D:/', '-v'];
+    expect(promptConditionMatch(process)).toBeFalsy();
+
+    process.argv = ['a', 'b', '-v', 'get-images', '-p', 'D:/'];
+    expect(promptConditionMatch(process)).toBeFalsy();
+  });
 });
