4872e079258620232023ac481da968811a59b1df
duyphaphach <duy@passcareer.io>
2020-02-27 16:32:47 +0700

    U1-90 Add log to debug CI

--- tests/unit/helpers/extend-logger.test.js
+++ tests/unit/helpers/extend-logger.test.js
@@ -49,9 +49,10 @@ describe('Helper - Function extend-debug-logger', () => {
       '\u001b[35maction:mock-action: Message content debug level with meta data',
     ];
     console._stdout.write = oldStdout;
+    console.log(logs)
     expect(logs[0]).toEqual(expectedReformatedLogs[0]);
     expect(logs[1]).toEqual(expectedReformatedLogs[1]);
-    expect(logs[2].indexOf(expectedReformatedLogs[2])).not.toEqual(-1);
-    expect(logs[3].indexOf(expectedReformatedLogs[3])).not.toEqual(-1);
+    expect(logs[2]).toEqual(expect.stringContaining(expectedReformatedLogs[2]));
+    expect(logs[3]).toEqual(expect.stringContaining(expectedReformatedLogs[3]));
   });
 });
