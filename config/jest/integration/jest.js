cff2688fa5561486a85dc290fd2189bece9de0d1
Duy Phạm <duy@passcareer.io>
2021-05-25 00:21:39 +0700

    Update jest config to ignore strip-ansi from babel transform because it is using ESM

--- config/jest/integration/jest.js
+++ config/jest/integration/jest.js
@@ -1,6 +1,9 @@
 /* eslint quotes: ["error", "single"] */
+const esModules = ['strip-ansi'].join('|');
+
 module.exports = {
   rootDir: './../../..',
+  transformIgnorePatterns: [`/node_modules/(?!${esModules})`],
   cacheDirectory: '<rootDir>/.cache/jest',
   testMatch: ['<rootDir>/tests/integration/**/*.test.js'],
 };
--- config/jest/jest.js
+++ config/jest/jest.js
@@ -1,13 +1,10 @@
 /* eslint quotes: ["error", "single"] */
-const esModules = ['strip-ansi'].join('|');
-
 module.exports = {
   rootDir: './../..',
   verbose: true,
   transform: {
     '.+.js?$': 'babel-jest',
   },
-  transformIgnorePatterns: [`/node_modules/(?!${esModules})`],
   projects: [
     '<rootDir>/config/jest/unit/jest.js',
     '<rootDir>/config/jest/integration/jest.js',
