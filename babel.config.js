003f69420976caebd24433e2ef8b56be1d7f43f8
duyphaphach <duyphamle97@gmail.com>
2021-11-26 17:49:44 +0700

    Update babel config

---
+++ babel.config.js
@@ -0,0 +1,41 @@
+export default {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: '14',
+        },
+      },
+    ],
+  ],
+  env: {
+    test: {
+      plugins: ['@babel/plugin-transform-modules-commonjs'],
+    },
+
+    development: {
+      plugins: [
+        '@babel/plugin-proposal-class-properties',
+        [
+          '@babel/plugin-transform-modules-commonjs',
+          {
+            allowTopLevelThis: true,
+          },
+        ],
+      ],
+    },
+
+    production: {
+      plugins: [
+        '@babel/plugin-proposal-class-properties',
+        [
+          '@babel/plugin-transform-modules-commonjs',
+          {
+            allowTopLevelThis: true,
+          },
+        ],
+      ],
+    },
+  },
+};
